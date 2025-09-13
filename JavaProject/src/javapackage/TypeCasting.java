a304900bb148d3702417dff78af67cbbe62c19a1
HARSHITHAPRIYAJAVAJI <javajiharshithapriya@gmail.com>
2025-09-13 12:32:54 +0530

    typec

--- JavaProject/src/javapackage/TypeCasting.java
+++ JavaProject/src/javapackage/TypeCasting.java
@@ -5,16 +5,13 @@ public class TypeCasting {
 	public static void main(String[] args) {
 		int x=55;
 		float y=x;
-		System.out.println(y);
+		System.out.println(y);  //55.0
 		double a = 30.97;
 		int b= (int)a;
-		System.out.println(b);
+		System.out.println(b);//30
 		float f = 123.45f;
 		long l = (long) f; 
-		System.out.println(l); 
-		
-	
-
+		System.out.println(l); //123
 	}
 
 }
