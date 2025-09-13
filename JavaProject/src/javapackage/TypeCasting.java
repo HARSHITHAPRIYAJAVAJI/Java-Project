fdd7126cc1320f2ad40c9d39beb5999093692dfa
HARSHITHAPRIYAJAVAJI <javajiharshithapriya@gmail.com>
2025-09-13 12:30:55 +0530

    typecasted

--- JavaProject/src/javapackage/TypeCasting.java
+++ JavaProject/src/javapackage/TypeCasting.java
@@ -1,11 +1,18 @@
 package javapackage;
 
 public class TypeCasting {
-
+//converting a variable of one type to another data type is TC
 	public static void main(String[] args) {
-		int x=5;
+		int x=55;
 		float y=x;
 		System.out.println(y);
+		double a = 30.97;
+		int b= (int)a;
+		System.out.println(b);
+		float f = 123.45f;
+		long l = (long) f; 
+		System.out.println(l); 
+		
 	
 
 	}
