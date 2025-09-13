2900676bfd6fef1e4e6645de40a500b94ea6fab4
HARSHITHAPRIYAJAVAJI <javajiharshithapriya@gmail.com>
2025-09-13 17:12:28 +0530

    prime numbers

---
+++ JavaProject/src/javapackage/Primenum.java
@@ -0,0 +1,23 @@
+package javapackage;
+
+public class Primenum {
+
+    public static void main(String[] args) {
+        System.out.println("Prime numbers from 1 to 100:");
+
+        for (int n = 2; n <= 100; n++) {
+            boolean isPrime = true;
+
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.println(n);
+            }
+        }
+    }
+}
