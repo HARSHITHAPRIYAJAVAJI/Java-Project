f265b5b5f3a367a877dacad5d1838d8af78c1223
HARSHITHAPRIYAJAVAJI <javajiharshithapriya@gmail.com>
2025-09-13 19:34:31 +0530

    palindrome

---
+++ JavaProject/src/javapackage/Palindrome.java
@@ -0,0 +1,22 @@
+package javapackage;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        String s = "madam";
+        boolean isPalindrome = true;
+        int n=s.length();
+
+        for (int i = 0; i < n / 2; i++) {
+            if (s.charAt(i) != s.charAt(n - i - 1)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(s + " is a palindrome.");
+        } else {
+            System.out.println(s + " is not a palindrome.");
+        }
+    }
+}
